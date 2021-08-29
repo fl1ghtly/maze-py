ade10ed260260a70c1929d157fdf521e31078674
James Nguyen <59295993+fl1ghtly@users.noreply.github.com>
2021-08-28 21:59:03 -0700

    changed array for maze

--- src/maze.py
+++ src/maze.py
@@ -1,18 +1,19 @@
 import numpy as np
 
 def make_empty_maze(size):
-    return np.full(size, None)
+    return [[[(x, y), 1] for x in range(size[0])] for y in range(size[1])]
 
 # Prints out the maze based upon its cells
 def render_maze(maze, character):
     for row in maze:
         for cell in row:
             # If cell can be entered print empty character
-            if cell == None:
+            cell_value = cell[1]
+            if cell_value == 0:
                 # TODO replace with better empty space character
                 print(".", end='')
             # Prints wall if cell can not be entered
-            elif cell == False:
+            elif cell_value == 1:
                 print("■", end='')
             # Prints the player character
             else:
