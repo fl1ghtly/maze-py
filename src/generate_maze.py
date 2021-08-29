283ec50f3b7b6f0378822cc2340b7365c3b00568
James Nguyen <59295993+fl1ghtly@users.noreply.github.com>
2021-08-28 19:44:30 -0700

    added rendering of maze

--- src/generate_maze.py
+++ src/generate_maze.py
@@ -3,10 +3,25 @@ import numpy as np
 def make_empty_maze(size):
     return np.full(size, True)
 
+# Prints out the maze based upon its cells
+def render_maze(maze):
+    for row in maze:
+        for cell in row:
+            # If cell can be entered print empty character
+            if cell:
+                # TODO replace with better empty space character
+                print(".", end='')
+            # Prints wall if cell can not be entered
+            else:
+                print("■", end='')
+        # Print new line for next row
+        print()
+
 def main():
     x_length = int(input("How wide should the maze be: "))
     y_length = int(input("How tall should the maze be: "))
     maze = make_empty_maze((x_length, y_length))
+    render_maze(maze)
 
 if __name__ == "__main__":
     main()
