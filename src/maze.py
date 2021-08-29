1d46c7aa73b1f3f54e4bc75915654317856c8905
CrazyConga TheGreat <hamburger95764@gmail.com>
2021-08-29 12:49:45 -0700

    maze algorithm implemented

--- src/maze.py
+++ src/maze.py
@@ -32,23 +32,44 @@ def convert_maze_for_render(maze, x_length, y_length):
                 render[2 * c.x + 1][2 * (c.y + 1)] = 'O'
     return render
 
-# Returns a list of valid neighboring cells
+# Returns a list of valid neighboring cell positions
 def get_neighbor(cell, x_length, y_length):
     neighbor_cells = [(cell.x, cell.y - 1), # North
                         (cell.x, cell.y + 1), # South
                         (cell.x + 1, cell.y), # East
                         (cell.x - 1, cell.y)] # West
 
+    cells = []
     for position in neighbor_cells:
         # Remove cell positions that are out of bounds
-        if not (0 < position[0] < x_length or 0 < position[1] < y_length):
-            neighbor_cells.remove(position)
+        if 0 <= position[0] < x_length and 0 <= position[1] < y_length:
+            cells.append(position)
             
-    return neighbor_cells
+    return cells
 
+def get_unvisited_cells(maze, cells):
+    unvisited = []
+    for pos in cells:
+        c =  maze[pos[0]][pos[1]]
+        if c.visited == False:
+            unvisited.append(c)
+    return unvisited            
 
-def create_maze_wall(maze):
-    pass
+def create_maze_wall(maze, x, y):
+    stack = deque()
+    start = maze[0][0]
+    start.visited = True
+    stack.append(start)
+
+    while stack:
+        cell = stack.pop()
+        cell_neighbors = get_unvisited_cells(maze, get_neighbor(cell, x, y))
+        if len(cell_neighbors) > 0:
+            stack.append(cell)
+            chosen = random.choice(cell_neighbors)
+            cell.remove_wall(chosen)
+            chosen.visited = True
+            stack.append(chosen)
 
 '''
 Player Sprites:
@@ -58,7 +79,7 @@ def main():
     x_length = int(input("How wide should the maze be: "))
     y_length = int(input("How tall should the maze be: "))
     maze = make_empty_maze((x_length, y_length))
-    create_maze_wall(maze)
+    create_maze_wall(maze, x_length, y_length)
     render = convert_maze_for_render(maze, x_length, y_length)
     render_maze(render)
 
