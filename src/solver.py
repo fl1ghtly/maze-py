64ca3dba69622b74721b4a99ba8c6a82d02772c0
James Nguyen <59295993+fl1ghtly@users.noreply.github.com>
2021-09-05 10:46:25 -0700

    added bfs algorithm

--- src/solver.py
+++ src/solver.py
@@ -7,6 +7,24 @@ def find_start(maze):
     for (x, y), cell in np.ndenumerate(maze):
         if cell == 'S':
             return x, y
+
+def breadth_first_search(maze, start):
+    queue = deque()
+    visited = [start]
+    queue.append(start)
+
+    while queue:
+        cell = queue.pop()
+        if maze[cell[0]][cell[1]] == 'F':
+            return cell
+
+        neighbors = get_adjacent(cell, len(maze[0]), len(maze))
+        for neighbor in neighbors:
+            if neighbor not in visited:
+                visited.append(neighbor)
+                queue.append(neighbor)
+        
+
 def main(file):
     maze_arr = converter.svg_to_array(file)
     np_maze = np.asarray(maze_arr)
