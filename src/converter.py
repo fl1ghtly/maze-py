a8371230ee4a90e6dbfc59b868096193f3d475a8
James Nguyen <59295993+fl1ghtly@users.noreply.github.com>
2021-09-05 00:45:09 -0700

    added ability to get dimensions from an svg of maze

--- src/converter.py
+++ src/converter.py
@@ -11,6 +11,40 @@ def convert_svg(file):
     renderPM.drawToFile(draw, buff, fmt='png')
     return buff
 
+# Gets the x by y dimension size of a maze
+def get_dimensions(file):
+    with open(file, 'r') as f:
+        lines = f.readlines()
+        # maximum is guaranteed to be above or at 0
+        x_max = 0
+        x_count = 0
+
+        y_max = 0
+        y_count = 0
+        y_passed_max = False
+        for line in lines:
+            x = regex.search(r'x=\K"(.*?)"', line)
+            y = regex.search(r'y=\K"(.*?)"', line)
+
+            if y:
+                if int(y.group(1)) >= y_max:
+                    y_max = int(y.group(1))
+                else:
+                    y_passed_max = True
+
+                if not y_passed_max:
+                    y_count += 1
+            if x:
+                if int(x.group(1)) > x_max:
+                    x_max = int(x.group(1))
+                    x_count += 1
+
+
+        height = y_count
+        # Count the last x that was missed
+        width = x_count + 1
+        return width, height
+
 # Converts a premade Maze SVG File to 
 # an array
 def svg_to_array(file):
