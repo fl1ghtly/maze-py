e0c31bd771d1a83c45ef01dd2f330a152011d3db
James Nguyen <59295993+fl1ghtly@users.noreply.github.com>
2021-09-05 10:45:41 -0700

    added main function

--- src/solver.py
+++ src/solver.py
@@ -2,3 +2,12 @@ from collections import deque
 from generator import get_adjacent
 import converter
 import numpy as np
+
+def main(file):
+    maze_arr = converter.svg_to_array(file)
+    np_maze = np.asarray(maze_arr)
+    start = find_start(np_maze)
+    breadth_first_search(np_maze, start)
+
+if __name__ == '__main__':
+    main('maze.svg')
