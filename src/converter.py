356e977d62dd5da65a8ef20f2cc0639842ef40e7
James Nguyen <59295993+fl1ghtly@users.noreply.github.com>
2021-09-04 21:29:28 -0700

    added ability to convert maze SVGs to array

--- src/converter.py
+++ src/converter.py
@@ -1,6 +1,7 @@
 from reportlab.graphics import renderPM
 from svglib.svglib import svg2rlg
 from io import BytesIO
+import regex
 
 # Converts a SVG file to a png file
 # Returns a python file object
@@ -10,5 +11,20 @@ def convert_svg(file):
     renderPM.drawToFile(draw, buff, fmt='png')
     return buff
 
+# Converts a premade Maze SVG File to 
+# an array
+def svg_to_array(file, color_dict):
+    maze = []
+    with open(file, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            color = regex.search(r'fill=\K"(.*?)"', line)
+            if color:
+                maze.append(color_dict.get(color.group(1), None))
+    return maze
+
 if __name__ == '__main__':
-    convert_svg('maze.svg')
+    svg_map = {'black': 'W', 'white': 'O', 'red': 'F',
+               'green': 'S', 'orange': 'P'}
+    test = svg_to_array('maze.svg', svg_map)
+    pass
