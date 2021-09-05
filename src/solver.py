f3eacf3a2a2be8b47e7bb1578aa52bf7a889ed4f
James Nguyen <59295993+fl1ghtly@users.noreply.github.com>
2021-09-05 10:46:03 -0700

    added ability to find start position

--- src/solver.py
+++ src/solver.py
@@ -3,6 +3,10 @@ from generator import get_adjacent
 import converter
 import numpy as np
 
+def find_start(maze):
+    for (x, y), cell in np.ndenumerate(maze):
+        if cell == 'S':
+            return x, y
 def main(file):
     maze_arr = converter.svg_to_array(file)
     np_maze = np.asarray(maze_arr)
