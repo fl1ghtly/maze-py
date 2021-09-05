b45eb7ce3bf41438cc84d9810929ef5041e63637
James Nguyen <59295993+fl1ghtly@users.noreply.github.com>
2021-09-04 21:31:11 -0700

    rearrangement of variables

--- src/converter.py
+++ src/converter.py
@@ -13,18 +13,17 @@ def convert_svg(file):
 
 # Converts a premade Maze SVG File to 
 # an array
-def svg_to_array(file, color_dict):
+def svg_to_array(file):
     maze = []
+    svg_map = {'black': 'W', 'white': 'O', 'red': 'F',
+               'green': 'S', 'orange': 'P'}
     with open(file, 'r') as f:
         lines = f.readlines()
         for line in lines:
             color = regex.search(r'fill=\K"(.*?)"', line)
             if color:
-                maze.append(color_dict.get(color.group(1), None))
+                maze.append(svg_map.get(color.group(1), None))
     return maze
 
 if __name__ == '__main__':
-    svg_map = {'black': 'W', 'white': 'O', 'red': 'F',
-               'green': 'S', 'orange': 'P'}
-    test = svg_to_array('maze.svg', svg_map)
-    pass
+    svg_to_array('maze.svg')
