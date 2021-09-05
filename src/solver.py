5ae4720e716a54d6ef3ae29f56b99e2f318f22d1
James Nguyen <59295993+fl1ghtly@users.noreply.github.com>
2021-09-05 14:32:19 -0700

    updated main function + comments

--- src/solver.py
+++ src/solver.py
@@ -3,6 +3,7 @@ import generator
 import converter
 import numpy as np
 
+# Finds where the maze begins
 def find_start(maze):
     for (x, y), cell in np.ndenumerate(maze):
         if cell == 'S':
@@ -23,18 +24,25 @@ def breadth_first_search(maze, start):
         neighbors = generator.get_adjacent(cell, len(maze[0]), len(maze))
         for neighbor in neighbors:
             if maze[neighbor[1]][neighbor[0]] != 'W':
-            if neighbor not in visited:
-                visited.append(neighbor)
+                if neighbor not in visited:
+                    visited.append(neighbor)
                     new_path = list(path)
                     new_path.append(neighbor)
                     queue.append(new_path)
         
 
 def main(file):
+    # Converts an SVG to a maze format
     maze_arr = converter.svg_to_array(file)
+
     np_maze = np.asarray(maze_arr)
     start = find_start(np_maze)
-    breadth_first_search(np_maze, start)
+    path = breadth_first_search(np_maze, start)
+    maze_solution = generator.draw_path(maze_arr, path)
+    
+    svg_map = {'W': 'black', 'O': 'white', 'F': 'red',
+               'S': 'green', 'P': 'orange'}
+    generator.render_svg(maze_solution, svg_map, 'solution.svg')
 
 if __name__ == '__main__':
     main('maze.svg')
